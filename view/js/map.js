faa39f93ca7add1a2e5624c6da55103b8df88441
张瑶睿 <1059109666@qq.com>
2018-06-14 19:23:43 +0800

    hover事件 区分拥有着

--- view/js/map.js
+++ view/js/map.js
@@ -3,7 +3,8 @@ $(window).on('load', function(){
     {"time": "13:20", "number": 3}, {"time": "13:10", "number": 4}, {"time": "13:00", "number": 5},
     {"time": "12:50", "number": 3}, {"time": "12:40", "number": 4}, {"time": "12:30", "number": 5},
     {"time": "12:20", "number": 3}, {"time": "12:10", "number": 4}, {"time": "12:00", "number": 5}]);
-
+    
+    id = "哈哈哈";
     backNum = 0;
 	updateNum();
 	$(".lucky-number").find(".right").click(function(){
@@ -22,6 +23,7 @@ $(window).on('load', function(){
 
 	$(".main").width($(window).width());
 	$(".main").height($(window).height() - 40);
+	$("#infor").hide();
 
 	canvas=document.getElementById('map');
 	can = document.createElement("canvas");
@@ -29,16 +31,24 @@ $(window).on('load', function(){
 	bg_img = new Image();
     bg_img.src = "images/background.png";
     scale = 1.5;//图像显示缩放比例
-    controlScale = 1.5;//拖动鼠标时地图跟随距离
+    controlScale = 1.0;//拖动鼠标时地图跟随距离
     n = 6 + 6;//number of resources: map + road
     offsetX = 20;
     offsetY = 10;
     maps = new Array(6);
-    posSet = //显示数字，序号1~6（0代表无六边形），横位置，纵位置, 左上路径（0无，1有），左上角建筑（0无，1占位，2乡村，3城市 ），上路径，右上建筑，右上路径
-              [[17, 5, 0,-1, 1, 1, 1, 3, 1],
-               [12, 4,-1,-1, 0, 1, 1, 2, 1],
-               [ 0, 0, 1,-1, 1, 1, 0, 0, 0],
-               [ 0, 0, 0, 0, 0, 1, 1, 2, 0]];
+    //显示数字，序号1~6（0代表无六边形），横位置，纵位置, 左上路径（0无，1可建筑，2已有），左上角建筑（0无，1占位，2乡村，3城市 ），上路径，右上建筑，右上路径
+    posSet = ([{"mapInfor":[17, 5, 0, 0, 1, 1, 0, 1, 0], "owner":["小可爱", "小仙女", "#", "哈哈哈", "#"]},
+               {"mapInfor":[16, 5,-1,-1, 1, 1, 0, 1, 0], "owner" : ["小可爱",  "小仙女",  "哈哈哈","哈哈哈",  "#"]},
+               {"mapInfor":[12, 2,-1, 0, 1, 1, 2, 2, 2],"owner":["小可爱",  "小仙女",  "哈哈哈","哈哈哈",  "小仙女"]}, 
+               {"mapInfor":[1, 0, 0,-1, 0, 0, 0, 0, 0], "owner" : ["#",  "#", "#", "#", "#"]},
+               {"mapInfor":[2, 0, 1,-1, 1, 0, 0, 0, 0], "owner":["#",  "#", "#", "#", "#"]}, 
+               {"mapInfor":[3, 0, 1, 0, 1, 1, 0, 0, 0], "owner" : ["#",  "#", "#", "#", "#"]},
+               {"mapInfor":[4, 0, 0, 1, 0, 1, 2, 3, 1], "owner":["#",  "#", "小可爱", "哈哈哈", "#"]}, 
+               {"mapInfor":[5, 0,-1, 1, 0, 1, 0, 1, 0], "owner" : ["#",  "小仙女", "#", "#", "#"]},
+               {"mapInfor":[6, 0,-2, 1, 0, 0, 1, 2, 1], "owner":["#",  "#", "#", "哈哈哈", "#"]},
+               {"mapInfor":[7, 0,-2, 0, 0, 0, 1, 2, 1], "owner" : ["#",  "#", "#", "哈哈哈", "#"]},         
+               {"mapInfor":[8, 0,-2,-1, 0, 0, 0, 0, 1], "owner":["#", "#", "#", "#", "#"]}, 
+               {"mapInfor":[9, 0,-1,-2, 0, 0, 0, 0, 1], "owner" : ["#", "#", "#", "#", "#"]}]);
     for (var i = 0; i < 6; i++){
     	maps[i] = new Image();
     }
@@ -64,6 +74,10 @@ $(window).on('load', function(){
     if(oriX > 0)
     	oriX = 0;
     oriY = -offsetY * 72 * scale + $(window).height()/2;
+
+    x = new Array();
+    y = new Array();
+    //储存六边形坐标
     if(oriY > 0)
     	oriY = 0;
 	bg_img.onload = function () {
@@ -86,6 +100,10 @@ $(window).on('load', function(){
             }
         };
     }
+    svg = $("#mapElement")[0];
+    $(svg).width($(window).width());
+    $(svg).height($(window).height());
+
     svgContainer = d3.select("#mapElement")
     .attr("width",$(window).width())
     .attr("height", $(window).height());
@@ -93,10 +111,21 @@ $(window).on('load', function(){
 
     svgContainer.on("mousedown", function(ev){  
         var e = ev||event;  
-        x = e.clientX;  
-        y = e.clientY;  
-        drag(x,y);  
+        mx = e.clientX;  
+        my = e.clientY;  
+        mouseDrag(mx, my);  
     })
+
+    hx = 0;
+    hy = 0;
+    svg.addEventListener("touchstart", function(event){  
+        if(event.touches){
+        	var e = event.touches[0];  
+	        hx = e.clientX;  
+	        hy = e.clientY;  
+	        handDrag();  
+	    }
+    }, false)
 })
 
 function updateNum(){
@@ -133,62 +162,67 @@ function updateNum(){
     ctx.fillRect(-oriX, -oriY, $(window).width(),$(window).height() - 40); 
     for(var i = 0; i < posSet.length; i++){
     	if(scale < 1.0)
-    		x = (23 + (offsetX+ posSet[i][2]) * 64 )* 1.0* scale
+    		x[i] = (23 + (offsetX+ posSet[i].mapInfor[2]) * 64 )* 1.0* scale
     	else
-    		x = (27 + (offsetX+ posSet[i][2]) * 64 )* 1.0* scale;
-    	if(posSet[i][2]%2 == 0)
-    		y = ((posSet[i][3] + offsetY) * 72 - 1) * scale;
+    		x[i] = (26 + (offsetX+ posSet[i].mapInfor[2]) * 64 )* 1.0* scale;
+    	if(posSet[i].mapInfor[2]%2 == 0)
+    		y[i] = ((posSet[i].mapInfor[3] + offsetY) * 72 - 2) * scale;
         else
-        	y = ((posSet[i][3] + offsetY) * 72  + 34) * scale;
-        if(x < -oriX - 80 * scale || x > -oriX + $(window).width() + 10 * scale || y < -oriY - 75 * scale || y > -oriY + $(window).height())
+        	y[i] = ((posSet[i].mapInfor[3] + offsetY) * 72  + 34) * scale;
+
+        if(x[i] < -oriX - 80 * scale || x > -oriX + $(window).width() + 10 * scale || y < -oriY - 75 * scale || y > -oriY + $(window).height()){
+        	i--;
         	continue;
-        if(posSet[i][1]!= 0){
-            ctx.drawImage(maps[posSet[i][1] - 1], x, y, 74 * scale, 66 * scale);
+        }
+
+        if(posSet[i].mapInfor[1]!= 0){
+            ctx.drawImage(maps[posSet[i].mapInfor[1] - 1], x[i], y[i], 75 * scale, 66 * scale);
             ctx.beginPath();  
-            ctx.arc(x + 36*scale, y + 32*scale, 12 * scale,Math.PI*2,false);  
+            ctx.arc(x[i] + 36*scale, y[i] + 32*scale, 12 * scale,Math.PI*2,false);  
 	        ctx.fillStyle="rgba(237,239,243,0.9)";//背景色  
 	        ctx.fill();  
 	        ctx.strokeStyle="rgba(237,239,243,0.45)";//半透明背景色  
 	        ctx.stroke();  
 	        ctx.font= 12 * scale + "px" +  " 微软雅黑";
 	        ctx.fillStyle="rgba(50,50,50,0.9)";
-	        if(posSet[i][0] < 10)
-	            ctx.fillText(posSet[i][0] ,x + 32 * scale,y + 37 * scale);
+	        if(posSet[i].mapInfor[0] < 10)
+	            ctx.fillText(posSet[i].mapInfor[0] ,x[i] + 33 * scale,y[i] + 37 * scale);
 	        else
-	        	ctx.fillText(posSet[i][0] ,x + 29 * scale,y + 37 * scale);
+	        	ctx.fillText(posSet[i].mapInfor[0] ,x[i] + 29 * scale,y[i] + 37 * scale);
 	    }
         //绘制六边形
-        
-        if(posSet[i][4]){
-        	ctx.drawImage(road[3], x - 9 * scale, y - 2 * scale, 28 * scale, 37 * scale);
-        	lineMask(x - 9 * scale, y + 29 * scale, 30 * scale, 10 * scale, -60, 1, i);
+    }
+    var valid = i;
+    for(var i = 0; i < valid; i++){      
+        if(posSet[i].mapInfor[4] == 2){
+        	ctx.drawImage(road[3], x[i] - 8 * scale, y[i] - 2 * scale, 28 * scale, 37 * scale);
+        	lineMask(x[i] - 7 * scale, y[i] + 28 * scale, 36 * scale, 10 * scale, -60, 2, i);
         }
-        else
-        	lineMask(x - 9 * scale, y + 29 * scale, 30 * scale, 10 * scale, -60, 0, i);
-        if(posSet[i][6]){
-        	ctx.drawImage(road[4], x + 19* scale, y - 8 * scale, 36 * scale, 10 * scale);
-        	lineMask(x + 24* scale, y - 7 * scale, 32 * scale, 10 * scale, 0, 1, i);
+        else if(posSet[i].mapInfor[4] == 1)
+        	lineMask(x[i] - 7 * scale, y[i]+ 28 * scale, 36 * scale, 10 * scale, -60, 1, i);
+        if(posSet[i].mapInfor[6] == 2){
+        	ctx.drawImage(road[4], x[i] + 19* scale, y[i]- 8 * scale, 36 * scale, 10 * scale);
+        	lineMask(x[i] + 19* scale, y[i]- 7 * scale, 36 * scale, 10 * scale, 0, 2, i);
         }
-        else
-        	lineMask(x + 24* scale, y - 7 * scale, 32 * scale, 10 * scale, 0);
-        if(posSet[i][8]){
-        	ctx.drawImage(road[5], x + 56* scale, y - 2 * scale, 27 * scale, 37 * scale);
-        	lineMask(x + 68* scale, y + 3 * scale , 30 * scale, 10 * scale, 60, 1, i);
+        else if (posSet[i].mapInfor[6] == 1)
+        	lineMask(x[i] + 19* scale, y[i]- 7 * scale, 36 * scale, 10 * scale, 0, 1, i);
+        if(posSet[i].mapInfor[8] == 2){
+        	ctx.drawImage(road[5], x[i] + 57* scale, y[i]- 2 * scale, 27 * scale, 37 * scale);
+        	lineMask(x[i] + 65* scale, y[i] - 2*scale, 36 * scale, 10 * scale, 60, 2, i);
         }
-        else
-        	lineMask(x + 68* scale, y + 3 * scale, 30 * scale, 10 * scale, 60, 0);
+        else if(posSet[i].mapInfor[8] == 1)
+        	lineMask(x[i] + 65* scale, y[i] - 2*scale, 36* scale, 10 * scale, 60, 1, i);
+    }//绘制道路
 
-        //绘制道路
-        
-
-        if(posSet[i][5]){
-        	ctx.drawImage(road[posSet[i][5] - 1], x + 6 * scale, y - 12 * scale, 20 * scale, 20 * scale);
-        	circleMask(x + 6 * scale, y - 12 * scale, 0, i);
+    for(var i = 0; i < valid; i++){  
+        if(posSet[i].mapInfor[5]){
+        	ctx.drawImage(road[posSet[i].mapInfor[5] - 1], x[i] + 6 * scale, y[i]- 12 * scale, 20 * scale, 20 * scale);
+        	circleMask(x[i] + 6 * scale, y[i]- 12 * scale, 0, i);
         }
 
-        if(posSet[i][7]){
-        	ctx.drawImage(road[posSet[i][7] - 1], x + 49 * scale, y - 12 * scale, 20 * scale, 20 * scale);
-        	circleMask(x + 49 * scale, y - 12 * scale, 1, i);
+        if(posSet[i].mapInfor[7]){
+        	ctx.drawImage(road[posSet[i].mapInfor[7] - 1], x[i] + 49 * scale, y[i]- 12 * scale, 20 * scale, 20 * scale);
+        	circleMask(x[i] + 49 * scale, y[i]- 12 * scale, 1, i);
         }
     }      
 } 
@@ -238,26 +272,46 @@ function resize(){
 }
 
 //拖拽函数  
-function drag(x,y){  
-    // 当画布上有多个路径时，isPointInPath只能判断最后那一个绘制的路径  
-        //路径正确，鼠标移动事件  
+function mouseDrag(mx, my){  
+    // 当画布上有多个路径时, isPointInPath只能判断最后那一个绘制的路径  
+        //路径正确, 鼠标移动事件  
     svgContainer.on("mousemove", function(ev){  
         var e = ev||event;   
         var ax = e.clientX;  
         var ay = e.clientY; 
         
-        if(oriX + ax - x < 0 && oriY + ay - y < 0 && oriX + ax - x > -4000* scale && oriY + ay - y > -1800 * scale)  {
-            drawScene(controlScale*(ax - x),controlScale* (ay - y));  
-            oriX = oriX + controlScale * (ax - x); oriY = oriY + controlScale * (ay - y);
+        if(oriX + ax -mx< 0 && oriY + ay - my < 0 && oriX + ax -mx> -4000* scale && oriY + ay - my > -1800 * scale)  {
+            drawScene(controlScale*(ax - mx),controlScale* (ay - my));  
+            oriX = oriX + controlScale * (ax - mx); oriY = oriY + controlScale * (ay - my);
         }
-        x = ax; y = ay;
+       mx= ax; my = ay;
         console.log("oriX:" + oriX + "  oriY:" + oriY);
     })
     //鼠标移开事件  
-    svgContainer.on("mouseup", function(ev){  
+    svgContainer.on("mouseup", function(){  
         svgContainer.on("mousemove", null);  
         svgContainer.on("mouseup", null);  
     })
+}
+
+function handDrag(){ 
+     svg.addEventListener("touchmove", function(event){  
+        var e = event.touches[0];   
+        var ax = e.clientX;  
+        var ay = e.clientY; 
+        
+        if(oriX + ax -hx< 0 && oriY + ay - hy < 0 && oriX + ax -hx> -4000* scale && oriY + ay - hy > -1800 * scale)  {
+            drawScene(controlScale*(ax - hx),controlScale* (ay - hy));  
+            oriX = oriX + controlScale * (ax - hx); oriY = oriY + controlScale * (ay - hy);
+        }
+        hx= ax; hy = ay;
+        console.log("oriX:" + oriX + "  oriY:" + oriY);
+    }, false)
+    //鼠标移开事件  
+    svg.addEventListener("touchend", function(){  
+        svgContainer.on("touchmove", null);  
+        svgContainer.on("touchend", null);  
+    }, false)
 }
 
 function circleMask(x, y, structType, index){
@@ -268,26 +322,65 @@ function circleMask(x, y, structType, index){
 		.style("fill", "rgba(255, 255, 255, 0)")
 		.style("cursor", "pointer")
 		.attr("index", index)
-		.attr("structType", structType);
+		.attr("structType", structType)
+		.attr("owner", posSet[index].owner[1 + structType*2]);
+	if(circle.attr("owner") != id && circle.attr("owner") != "#")circle.style("fill","rgba(67, 58, 20, 0.2)");
 	circle.on("mouseover", function(){
-		d3.select(this).transition(300).style("fill", "rgba(255, 255, 200, 0.2)");
+		if(typeof timer != "undefined")clearTimeout(timer);
+		var choosenCircle = d3.select(this);
+		timer = setTimeout(function(){
+			if(circle.attr("owner") != id && circle.attr("owner") != "#")
+				choosenCircle.transition(200).style("fill", "rgba(67, 58, 20, 0.3)");
+			else
+			   	choosenCircle.transition(200).style("fill", "rgba(255, 255, 200, 0.2)");
+			
+			if(choosenCircle.attr("owner") != "#"){
+				var top = parseInt(choosenCircle.attr("cy")) + 40 + 10 * scale;
+			    var left =  parseInt(choosenCircle.attr("cx"));
+			    $("#infor").css("top", top + "px");
+			    $("#infor").css("left", left + "px");
+                
+                if(posSet[choosenCircle.attr("index")].mapInfor[choosenCircle.attr("structType")*2 + 5] == 2){
+				    if(choosenCircle.attr("owner") == id)
+				        $("#infor").text("我的村庄");
+					else
+					    $("#infor").text("村庄拥有者： "+ choosenCircle.attr("owner"));
+				}
+				else{
+                    if(choosenCircle.attr("owner") == id)
+				        $("#infor").text("我的城市");
+					else
+					    $("#infor").text("城市拥有者： "+ choosenCircle.attr("owner"));
+				}
+				$("#infor").show();
+		    }
+		},100)
 		svgContainer.on("mousedown", null);
 	})
 	circle.on("mouseout", function(){
-		d3.select(this).transition(300).style("fill", "rgba(255, 255, 255, 0)");
+		if(typeof timer != "undefined")clearTimeout(timer);
+		timer = setTimeout(function(){
+			if(circle.attr("owner") != id && circle.attr("owner") != "#")
+				circle.style("fill","rgba(67, 58, 20, 0.2)");
+			else
+			   circle.transition(300).style("fill", "rgba(255, 255, 255, 0)");
+			$("#infor").hide();
+		}, 100)
+
 		svgContainer.on("mousedown", function(ev){  
             var e = ev||event;  
             x = e.clientX;  
             y = e.clientY;  
-            drag(x,y);  
+            mouseDrag(x,y);  
         })
 	})
 	circle.on("click", function(){
 		console.log("click");
 		//to do 
-		if(posSet[d3.select(this).attr("index")][d3.select(this).attr("structType")*2 + 5] == 2)
+		if(d3.select(this).attr("owner") != "#" && d3.select(this).attr("owner") != id)return;
+		if(posSet[d3.select(this).attr("index")].mapInfor[d3.select(this).attr("structType")*2 + 5] == 2)
 			$("#structCreate").find(".modal-body").text("你确定花费13单位粮食、6单位砖块、10单位矿石将此村庄扩建成城市吗？");
-		else if(posSet[d3.select(this).attr("index")][d3.select(this).attr("structType")*2 + 5] == 1)
+		else if(posSet[d3.select(this).attr("index")].mapInfor[d3.select(this).attr("structType")*2 + 5] == 1)
             $("#structCreate").find(".modal-body").text("你确定花费4单位木头、3单位砖块、9单位羊毛、6单位粮食在此地建设1个村庄吗？（前666位玩家可以免费建设2个村庄，剩余X/666）");
 		else
 			return;
@@ -307,24 +400,52 @@ function lineMask(x, y, width, height, angle, lineType, index){
 		.style("fill","rgba(255, 255, 200, 0.0)")
 		.style("cursor", "pointer")
 		.attr("index", index)
-		.attr("pos", angle / 60);
-	line.on("mouseover", function(){
-		if(lineType)
-		    d3.select(this).transition(300).style("fill", "rgba(255, 255, 200, 0.08)");
-		else
-			d3.select(this).transition(300).style("fill", "rgba(188, 176, 134, 0.5)");
-		svgContainer.on("mousedown", null)
-	})
+		.attr("pos", angle / 60)
+		.attr("owner", posSet[index].owner[2 + angle/60]);
+
+	if(lineType == 2 && line.attr("owner") != id)line.style("fill","rgba(67, 58, 20, 0.1)");
+	if(lineType){
+		line.on("mouseover", function(){
+			if(typeof timer != "undefined")clearTimeout(timer);
+			var choosenLine = d3.select(this);
+			timer = setTimeout(function(){
+				if(lineType == 2){
+					if(choosenLine.attr("owner") != id)
+				        choosenLine.transition(200).style("fill", "rgba(67, 58, 20, 0.2)");
+				    else
+				    	choosenLine.transition(200).style("fill", "rgba(255, 255, 200, 0.08)");
+				    svgContainer.on("mousedown", null)
+					var top = parseInt(choosenLine.attr("y")) + 40 + 10 * scale;
+					var left =  parseInt(choosenLine.attr("x"));
+					$("#infor").css("top", top + "px");
+					$("#infor").css("left", left + "px");
+					if(choosenLine.attr("owner") == id)
+						 $("#infor").text("我的道路");
+					else
+					    $("#infor").text("道路拥有者： "+ choosenLine.attr("owner"));
+					$("#infor").show();
+				}
+				else if(lineType == 1)
+					choosenLine.transition(200).style("fill", "rgba(188, 176, 134, 0.5)");
+				
+			},100)
+		})
+	}
 	line.on("mouseout", function(){
-		d3.select(this).transition(300).style("fill", "rgba(255, 255, 255, 0)");
+		if(typeof timer != "undefined")clearTimeout(timer);
+		if(lineType == 2 && line.attr("owner") != id)
+			line.style("fill","rgba(0, 0, 0, 0.1)");
+		else
+			line.transition(200).style("fill", "rgba(255, 255, 255, 0)");
+		$("#infor").hide();
 		svgContainer.on("mousedown", function(ev){  
 			var e = ev||event;  
             x = e.clientX;  
             y = e.clientY;  
-            drag(x,y);  
+            mouseDrag(x,y);  
 		})
 	})
-	if(!lineType){
+	if(lineType == 1){
 		line.on("click", function(){
 			console.log("click_line");
 			//to do
@@ -332,4 +453,5 @@ function lineMask(x, y, width, height, angle, lineType, index){
 			$('#structCreate').modal('show');
 		})
 	}
+
 }
